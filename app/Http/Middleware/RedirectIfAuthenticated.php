f56e941359899bfcd532b9903632a5047fa679ef
Kirby Capangpangan <kcgerona@gmail.com>
2018-10-13 13:57:02 +0800

    check from middlware

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Firebase\JWT\JWT;
 
 class RedirectIfAuthenticated
 {
@@ -18,9 +19,39 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return $this->isFromZendesk($request,Auth::user())?: redirect('/home');
         }
 
         return $next($request);
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function isFromZendesk($request, $user)
+    {
+
+        if ($request->has("return_to")) {
+            $key       = getenv("ZENDESK_KEY");
+            $subdomain = getenv("ZENDESK_SUBDOMAIN");
+            $now       = time();
+
+            $token = array(
+                "jti"   => bcrypt($now . rand()),
+                "iat"   => $now,
+                "name"  => $user->name,
+                "email" => $user->email
+            );
+
+            $jwt = JWT::encode($token, $key);
+            $location = "https://" . $subdomain . ".zendesk.com/access/jwt?jwt=" . $jwt;
+            $location .= "&return_to=" . urlencode($request->return_to);
+
+            return redirect()->away($location);
+        }
     }
 }
