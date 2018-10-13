83aafe0763cab192743b5d515f33610c7bde665f
Kirby Capangpangan <kcgerona@gmail.com>
2018-10-13 10:07:39 +0800

    test

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email',128)->unique();
+            $table->mediumText('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
