519ea615f0e6985b30ab382089a427a113011769
Kirby Capangpangan <kcgerona@gmail.com>
2018-10-13 17:11:20 +0800

    asdf

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -56,8 +56,9 @@ class LoginController extends Controller
      */
     public function showLoginForm(Request $request)
     {
+        
         if ($request->has('redirect_to')) {
-            session(['redirect_to' => $request->redirect_to]);
+            session()->put('redirect_to',$request->redirect_to);
         }
         return view('auth.login');
     }
@@ -71,7 +72,7 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
-dd(session()->all());
+
         if (session()->has("return_to")) {
 
             $location = $this->service->buildRedirect(session('return_to'));
